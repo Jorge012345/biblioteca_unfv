cb0a6d35b9d1b7ae7874baa6ebb4e7ac3442b8dc
Jorge012345 <kakaroto_99_m@hotmail.com>
2022-03-09 23:23:39 -0500

    cambios

---
+++ Terrazas Jorge/assets/js/solicitud.js
@@ -0,0 +1,5 @@
+const d=document
+
+const $form=document.getElementById("form-solicitud")
+
+$form.action="../assets/php/solicitud.php"
--- Terrazas Jorge/assets/php/reclamos.php
+++ Terrazas Jorge/assets/php/reclamos.php
@@ -12,22 +12,26 @@
         <?php
             $nombre=$_POST['txtNombre'];
             $apellido=$_POST['txtApellido'];
-            $pass=$_POST['txtPassword'];
-            $codigo=$_POST['txtCodigo'];
-            $dni=$_POST['txtDni'];
             $correo=$_POST['txtCorreo'];
-            $genero=$_POST['genero'];
-            echo "<h1>Registro enviado con éxito</h1>";
+            $preg1=$_POST['miCheck'];
+            $preg2=$_POST['solucion'];
+            $preg3=$_POST['fecha'];
+            $preg4=$_POST['hora'];
+            $preg5=$_POST['txtComentario'];
+            $preg6=$_POST['foto'];
+            
+            echo "<h1>Reclamo enviado con éxito</h1>";
             echo "<div class='parrafos'>";
             echo "<p>Nombre:  ",$nombre,"</p>";
             echo "<p>Apellido:  ",$apellido,"</p>";
-            echo "<p>Contraseña:  ",$pass,"</p>";
-            echo "<p>Codigo:  ",$codigo,"</p>";
-            echo "<p>DNI:  ",$dni,"</p>";
             echo "<p>Correo:  ",$correo,"</p>";
-            echo "<p>Género:  ",$genero,"</p>";
+            echo "<p>Pregunta 1:  ",$preg1,"</p>";
+            echo "<p>Pregunta 2:  ",$preg2,"</p>";
+            echo "<p>Pregunta 3:  ",$preg3,"</p>";
+            echo "<p>Pregunta 4:  ",$preg4,"</p>";
+            echo "<p>Pregunta 5:  ",$preg5,"</p>";
+            echo "<p>Pregunta 6:  ",$preg6,"</p>";
             echo "</div>";
-            
         ?>
     </main>
 </body>
