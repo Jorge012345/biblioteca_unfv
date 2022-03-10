06cb1d54f08754b35957882fe58c7bc152697a35
Jorge012345 <kakaroto_99_m@hotmail.com>
2022-03-10 12:24:58 -0500

    carrusel

--- Terrazas Jorge/assets/js/carrusel.js
+++ Terrazas Jorge/assets/js/carrusel.js
@@ -5,23 +5,12 @@ const $nextBtn = d.querySelector(".slider-btns .next"),
   $slides = d.querySelectorAll(".slider-slide");
 
 let i = 0;
-d.addEventListener("click", (e) => {
-  if (e.target === $prevBtn) {
-    e.preventDefault();
-    $slides[i].classList.remove("active");
-    i--;
-    if (i < 0) {
-      i = $slides.length - 1;
-    }
-    $slides[i].classList.add("active");
-  }
-  if (e.target === $nextBtn) {
-    e.preventDefault();
+
+setInterval(()=>{
     $slides[i].classList.remove("active");
     i++;
     if (i >= $slides.length) {
       i = 0;
     }
     $slides[i].classList.add("active");
-  }
-});
+},4000)
