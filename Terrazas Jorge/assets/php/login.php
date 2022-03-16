af64c11341bd4abe968c1d2dea9859deb3ec55ed
Jorge012345 <kakaroto_99_m@hotmail.com>
2022-03-16 13:41:57 -0500

    xx

--- Terrazas Jorge/assets/php/login.php
+++ Terrazas Jorge/assets/php/login.php
@@ -3,7 +3,26 @@
     $pass=$_POST['txtPassword'];
     $tipo=$_POST['txtTipo'];
 
-   
+    /*
+    $connection=mysqli_connect("localhost","root","","biblioteca");
+
+    if(!$connection){
+        echo "Error: No se pudo conectar a MySQL. Error ";
+        die;
+    }
+
+    $query="SELECT * FROM usuarios WHERE mail = '$email' AND password = '$pass'";
+
+
+
+
+
+    if (($result = mysqli_query($connection, $query)) === false) {
+        die(mysqli_error($connection));
+    } 
+
+
+    */
     if($email=="admin@gmail.com" && $pass=="1234" && $tipo=="Administrador"){
         header("location:/BibliotecaUNFV/Terrazas%20Jorge/admin/home_admin.html");
     }else if($email=="estudiante@gmail.com" && $pass=="1234" && $tipo=="Estudiante"){
